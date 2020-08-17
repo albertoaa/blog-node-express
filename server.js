32d30d64e23a941690d30b1c6b2f7efcff74a065
Alberto Aragon <ronin8503@gmail.com>
2020-08-17 14:31:42 -0500

    adding first router and route for articles

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const articlesRouter = require('./routes/articles');
 const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use('/articles', articlesRouter);
+
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index ');
 });
 
 app.listen(5000);
