b53471dbe00457b253c46185583e43ea7f4635fb
Alberto Aragon <ronin8503@gmail.com>
2020-08-17 14:45:22 -0500

    render articles on index page

--- server.js
+++ server.js
@@ -7,7 +7,19 @@ app.set('view engine', 'ejs');
 app.use('/articles', articlesRouter);
 
 app.get('/', (req, res) => {
-  res.render('index ');
+  const articles = [
+    {
+      title: 'Test Article',
+      createdAt: new Date(),
+      description: 'Test description',
+    },
+    {
+      title: 'Test Article 2',
+      createdAt: new Date(),
+      description: 'Test description 2',
+    },
+  ];
+  res.render('index', { articles });
 });
 
 app.listen(5000);
