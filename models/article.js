388bde4e49bd085855e06667c5e2de085b5c239c
Alberto Aragon <ronin8503@gmail.com>
2020-08-17 17:43:53 -0500

    sanitize and show markdown content for an article

--- models/article.js
+++ models/article.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const marked = require('marked');
 const slugify = require('slugify');
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const domPurify = createDomPurify(new JSDOM().window);
 
 const articleSchema = new mongoose.Schema({
   title: {
@@ -23,11 +26,18 @@ const articleSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  sanitizeHTML: {
+    type: String,
+    required: true,
+  },
 });
 
 articleSchema.pre('validate', function () {
   if (this.title) {
     this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+  if (this.markdown) {
+    this.sanitizeHTML = domPurify.sanitize(marked(this.markdown));
   }
 });
 module.exports = mongoose.model('Article', articleSchema);
